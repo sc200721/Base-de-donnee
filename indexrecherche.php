9b79c0b0a42f9daf0eb9561b1fe681a0ff79e9ba
Shenomes <gangneuxsheno@gmail.com>
2026-04-22 15:15:02 +0200

    Update indexrecherche.php

--- indexrecherche.php
+++ indexrecherche.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO("mysql:host=localhost;dbname=ta_base;charset=utf8", "root", "");
+    $pdo = new PDO("mysql:host=localhost;dbname=moncoinvert;charset=utf8", "root", "");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
     die("Erreur de connexion");
